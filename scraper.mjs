83b357a950b7297ebba44bd451b282cda5b299a4
IgnaceB <ignacebernard@hotmail.com>
2023-08-09 09:26:36 +0200

    data scrap

---
+++ scraper.mjs
@@ -0,0 +1,86 @@
+import cheerio from 'cheerio'
+import axios from 'axios'
+import {load} from 'cheerio'
+import fs from 'fs'
+import client from './helpers/db.mjs'
+const playerColl=client.db('football').collection('player')
+
+
+let arrayData=[]
+let thisData={
+}
+
+const scrap = async () =>{ 
+	for (let j=1;j<606;j++){
+		console.log(`page nbr ${j}`)
+	try {
+		const request = await axios.request({
+			method:"GET",
+			url : `https://www.fifaindex.com/players/${j}`,
+			headers: {
+				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+			}
+		})
+
+		
+		const $=load(request.data)
+/*		const html= $('tbody>tr')*/
+		const html= $('[data-playerid]')
+
+/*		console.log($(html[0].children[1].children[0].children[0]))*/
+
+		for (let i = 0 ; i<html.length;i++){
+			let thisAttribute=$(html[i])
+			const content = thisAttribute
+			thisData={
+				name : $(html[i].children[3]).text().trim(),
+				ovrpot : $(html[i].children[2]).text().trim(),
+				country : $(html[i].children[1]).find('img').attr('alt'),
+				position : { first : $(html[i]).find('.link-position').attr('title'),
+							second : $(html[i]).find('.link-position:nth-child(2)').attr('title'),
+							third : $(html[i]).find('.link-position:nth-child(3)').attr('title')
+						},
+				age : $(html[i]).find('[data-title="Age"]').text().trim(),
+				team : $(html[i]).find('.link-team img').attr('alt'),
+				picture : $(html[i].children[0]).find('a.link-player img').attr('src'),
+				teamPicture : $(html[i]).find('a.link-team img').attr('src')
+			}
+			
+
+			let alreadyExists=await playerColl.find({name : thisData.name}).toArray()
+
+			
+
+			if(alreadyExists.length>0){
+				console.log("already Exists")
+  			}
+			else {
+				await playerColl.insertOne(thisData)
+				console.log(`${thisData.name.toString()} added to database`)
+			}
+
+/*			await playerColl.insertOne(thisData)
+			console.log(`data pushed ${thisData.name}`)*/
+		}
+	/*		if (thisAttribute.data-playerid!=undefined){
+						console.log(thisAttribute)
+					arrayData.push($html[i])
+			}*/
+
+	}
+	catch(error){
+		console.error(error)
+	}
+/*	console.log(arrayData)*/
+
+}
+/*	fs.writeFile('./data.json',JSON.stringify(arrayData),(err)=>{
+			if (err){
+				console.log(err)
+			}
+			else {
+				console.log("data written")
+			}
+		})*/
+}
+scrap()
--- server.mjs
+++ server.mjs
@@ -10,16 +10,28 @@ app.set('view engine','ejs')
 app.use('/static',express.static('public'))
 app.use(express.json())
 
+const playerColl=client.db('football').collection('player')
+
+
+
+/*const allPlayer= await playerColl.find(({}))*/
+
+/*console.log(allPlayer)*/
+
  const testConn = async () =>{
   try {
   await client.connect()
-  console.log('Connexion ok')}
+  console.log('Connexion ok')
+  const allPlayer= await playerColl.find({}).toArray()
+  console.log(allPlayer.length)
+}
   catch (err) {
     console.log(err)
   }
 };
 
  testConn()
+
 
  app.get('/',(req,res)=>{
  	res.render('main.ejs')
